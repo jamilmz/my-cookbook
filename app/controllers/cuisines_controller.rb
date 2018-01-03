2598f19d76021f9d39b37c85929482a763969dc8
Jamil Martinez <jamil.cmc@gmail.com>
2018-01-03 01:11:06 -0200

    Administrador cadastra cozinhas

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -1,6 +1,26 @@
 class CuisinesController < ApplicationController
+	
 	def show
 		@cuisine = Cuisine.find(params[:id])
 		@recipes = Recipe.where(cuisine_id: params[:id])
 	end
+
+	def new
+		@cuisine = Cuisine.new
+	end
+
+	def create
+		@cuisine = Cuisine.new(cuisine_params)
+
+		if @cuisine.save
+			redirect_to @cuisine
+		else
+			render 'new'
+		end
+	end
+
+	private
+	  def cuisine_params
+	  	params.require(:cuisine).permit(:name)
+	  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'home#index'
   
   resources :recipes, only: [:show, :new, :create]
-  resources :cuisines, only: [:show]
+  resources :cuisines, only: [:show, :new, :create]
   
 end

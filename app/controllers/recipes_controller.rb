c9a3e8243294e932b5f36397e8dc7a3c3b4bf894
Jamil Martinez <jamil.cmc@gmail.com>
2018-01-03 01:44:09 -0200

    Usuário deve cadastrar receitas corretamente

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,17 +8,17 @@ class RecipesController < ApplicationController
   end
 
   def create 
-  	title = params[:recipe][:title]
-  	recipe_type = params[:recipe][:recipe_type]
-    cuisine_id = params[:recipe][:cuisine_id]
-    difficulty = params[:recipe][:difficulty]
-    cook_time = params[:recipe][:cook_time]
-    ingredients = params[:recipe][:ingredients]
-    method = params[:recipe][:method]
+    @recipe = Recipe.new(recipe_params)
 
-    recipe = Recipe.new(title: title, recipe_type: recipe_type, cuisine_id: cuisine_id, difficulty: difficulty, cook_time: cook_time, ingredients: ingredients, method: method)
-    recipe.save
-
-    redirect_to recipe_path(recipe)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render 'new'
+    end
   end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(:title, :recipe_type, :cuisine_id, :difficulty, :cook_time, :ingredients, :method)
+    end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,11 @@
 class Recipe < ApplicationRecord
 	belongs_to :cuisine
+
+	validates :title, presence: true
+	validates :recipe_type, presence: true
+	validates :cuisine_id, presence: true 
+	validates :difficulty, presence: true
+	validates :cook_time, presence: true
+	validates :ingredients, presence: true
+	validates :method, presence: true
 end

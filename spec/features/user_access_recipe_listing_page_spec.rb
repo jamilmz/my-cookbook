9ae47f37dc77459fa7f8c04b7b85f76a7c7660c3
Jamil Martinez <jamil.cmc@gmail.com>
2018-01-17 21:55:40 -0200

    refactor in the listing teste

--- spec/features/user_access_recipe_listing_page_spec.rb
+++ spec/features/user_access_recipe_listing_page_spec.rb
@@ -3,13 +3,19 @@ require 'rails_helper'
 feature 'user sees latest recipes at home' do
   scenario 'the last 6' do
     user = create(:user)
+
     cuisine = create(:cuisine)
     recipe_type = create(:recipe_type)
+
+    recipe = create(:recipe, user: user, title: 'Camarão a milanesa',
+                    cuisine: cuisine, recipe_type: recipe_type)
     recipes = create_list(:recipe, 6, cuisine: cuisine,
-    recipe_type: recipe_type, user: user)
+                    recipe_type: recipe_type, user: user)
 
      login_as(user)
      visit root_path
+
+     expect(page).not_to have_link recipe.title
      expect(page).to  have_selector('div.recipe', count: 6)
 
   end
@@ -26,6 +32,6 @@ feature 'user sees latest recipes at home' do
 
     click_on 'Visualizar todas receitas'
 
-    expect(page).to have_selector('div.recipe', count: Recipe.all.count)
+    expect(page).to have_selector('div.recipe', count: 10)
   end
 end
